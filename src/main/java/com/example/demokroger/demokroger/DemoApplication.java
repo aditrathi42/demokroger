b8b75cf1ae4cc42bcd8b4a33595ac0eaed31fc21
Yogesh Jain <yogesh.jain@macys.com>
2023-03-31 19:26:57 +0530

    Implemented Logging

--- src/main/java/com/example/demokroger/demokroger/DemoApplication.java
+++ src/main/java/com/example/demokroger/demokroger/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.example.demokroger.demokroger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +19,8 @@ public class DemoApplication {
 
 	@RequestMapping(value = "/")
 	public String base() {
+
+		log.info("Employee with details");
 		return "App is working";
 	}
 
